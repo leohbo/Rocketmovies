fbee39215e63c7255ee2966b12d403add1b212bf
Leonardo <leohbo59@gmail.com>
2025-03-20 08:50:20 -0300

    import global style, theme, theme provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import {Details} from './Pages/Details/';
-
+import GlobalStyles from './styles/global';
+import theme from "./styles/theme"
+import { ThemeProvider } from 'styled-components';
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles/>
     <Details />
+    </ThemeProvider>
   </StrictMode>,
 )
