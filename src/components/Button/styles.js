8e6cc169d70712df0ca6dd79ac43d784d82a5695
Leonardo <leohbo59@gmail.com>
2025-03-31 08:57:33 -0300

    button style

---
+++ src/components/Button/styles.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+export const Container = styled.button`
+
+width: 100%;
+background-color: ${({theme}) => theme.COLORS.PINK};
+color: ${({theme}) => theme.COLORS.BACKGROUND_800};
+
+height: 56px;
+padding: 0 16px;
+border: 0;
+margin-top: 24px;
+border-radius: 10px;
+font-weight: 500;
+
+&:disabled {
+  opacity: 0.5;
+
+}
+
+`
