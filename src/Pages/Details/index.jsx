309bc1a84ef2a9386bcbe9bb9200128a2462dbac
Leonardo <leohbo59@gmail.com>
2025-04-04 10:06:14 -0300

    adjust button

--- src/Pages/Details/index.jsx
+++ src/Pages/Details/index.jsx
@@ -1,17 +1,13 @@
 import {Container} from "./styles.js"
 
 import {Button} from "../../components/Button"
+import {Header} from "../../components/Header"
 
 export function Details() {
-
-return(
-  <Container>
-<h1>Hello World!</h1>
-<span>Leonardo Oliveira</span>
-<Button title="Entrar" loading/>
-<Button title="Cadastrar"/>
-<Button title="Voltar"/>
-  </Container>
-
+  return (
+    <Container>
+      <Header />
+      <Button title="Voltar" />
+    </Container>
   )
 }
