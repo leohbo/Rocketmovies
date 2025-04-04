c4052331d1572886e38e4c414f32363d9fd1d8be
Leonardo <leohbo59@gmail.com>
2025-04-04 15:51:22 -0300

    import section and tag

--- src/Pages/Details/index.jsx
+++ src/Pages/Details/index.jsx
@@ -2,11 +2,20 @@ import {Container} from "./styles.js"
 
 import {Button} from "../../components/Button"
 import {Header} from "../../components/Header"
-
+import {Section} from "../../components/Section"
+import {Tag} from "../../components/Tag"
 export function Details() {
   return (
     <Container>
       <Header />
+      <Section title="Marcadores">
+      <Tag title="Ficção Científica"/>
+      <Tag title="Drama"/>
+      <Tag title="Família">
+      </Tag>
+
+
+      </Section>
       <Button title="Voltar" />
     </Container>
   )
