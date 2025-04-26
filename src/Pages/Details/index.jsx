76c5a641a1ba5668fd038f9388a69a322a2a0cf2
Leonardo <leohbo59@gmail.com>
2025-04-26 10:20:23 -0300

    add component ButtonText

--- src/Pages/Details/index.jsx
+++ src/Pages/Details/index.jsx
@@ -1,13 +1,16 @@
 import {Container} from "./styles.js"
 
 import {Button} from "../../components/Button"
+import {ButtonText} from "../../components/ButtonText"
 import {Header} from "../../components/Header"
 import {Section} from "../../components/Section"
 import {Tag} from "../../components/Tag"
+
 export function Details() {
   return (
     <Container>
       <Header />
+      <ButtonText title="Excluir filme"/>
       <Section title="Marcadores">
       <Tag title="Ficção Científica"/>
       <Tag title="Drama"/>
---
+++ src/components/ButtonText/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const Container = styled.button`
+
+background: none;
+color: ${({theme}) => theme.COLORS.PINK};
+border: none;
+font-size: 1.2rem;
+
+`
